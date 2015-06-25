023f3c0388711e889a7d8a2f3819971843627e71
Ztawh <amandahwatz@gmail.com>
2015-06-25 12:47:11 +0200

    Creating js
    The clock works

---
+++ clock.js
@@ -0,0 +1,21 @@
+function updateClock() {
+	var now = new Date();
+
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	var seconds = now.getSeconds();
+
+	if(hours < 10) {
+		hours = "0" + hours;
+	}
+	if(minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if(seconds < 10) {
+		seconds = "0" + seconds;
+	}
+
+	var elem = document.getElementById("clock");
+
+	elem.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
